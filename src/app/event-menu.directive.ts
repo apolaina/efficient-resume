7339a226e66e913f3bbdd9cf744ecffa5983323b
mgrisole <grisole.maxime@gmail.com>
2018-10-13 17:38:47 +0200

    [efficient-resume] Add event-menu-directive

--- src/app/event-menu.directive.ts
+++ src/app/event-menu.directive.ts
@@ -7,18 +7,48 @@ export class EventMenuDirective implements OnInit {
 
   constructor(private renderer: Renderer2, private elRef: ElementRef) { }
 
-  let toggleFlag = false;
+  private isOpen = false;
 
-  ngOnInit() {
+  openMenu(): void {
+    // console.log( 'openMenu' );
+    let elemParent = document.querySelector('.header');
+    let elemChild = document.querySelector('.header__menu');
+    elemChild.classList.add('header__menu--open');
+    elemParent.classList.add('header--open');
+  }
 
-    // this.renderer.listen(this.elRef.nativeElement, 'click', () => {
-    //   this.toggleFlag = (this.toggleFlag === true) ? false : true;
-    //   if(this.toggleFlag) {
-    //     this.renderer.addClass(this.elRef.nativeElement, 'header__menu--open');
-    //   } else {
-    //     this.renderer.removeClass(this.elRef.nativeElement, 'header__menu--open');
-    //   }
-	  // });
+  closeMenu(): void {
+    // console.log( 'closeMenu' );
+    let elemParent = document.querySelector('.header');
+    let elemChild = document.querySelector('.header__menu');
+    elemChild.classList.remove('header__menu--open');
+    elemParent.classList.remove('header--open');
+  }
+
+  ngOnInit(): void {
+
+    console.log( 'isOpen onInit', this.isOpen );
+
+    this.renderer.listen(this.elRef.nativeElement, 'click', () => {
+
+      let elemParent: Element = document.querySelector('.header');
+      let elemChild: Element = document.querySelector('.header__menu');
+
+      this.isOpen = elemParent.classList.contains('header--open') && elemChild.classList.contains('header__menu--open');
+
+      console.log( 'isOpen click', this.isOpen );
+
+      if(this.isOpen) {
+        this.closeMenu();
+        this.isOpen = false;
+        console.log( 'removeClass' );
+      } else {
+        this.openMenu();
+        this.isOpen = true;
+        console.log( 'addClass' );
+      }
+
+	  });
 
   }
 
