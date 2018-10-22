770ff0df3d3f0f03ba5d83c4461e2faa91e5a693
apolaina <polainalice@gmail.com>
2018-10-22 08:16:13 +0200

    [efficient-resume] Update event-menu-directive

--- src/app/event-menu.directive.ts
+++ src/app/event-menu.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, OnInit, Renderer2, ElementRef } from '@angular/core';
+import { Directive, HostListener, OnInit, Input } from '@angular/core';
 
 @Directive({
   selector: '[appEventMenu]'
@@ -6,29 +6,26 @@ import { Directive, OnInit, Renderer2, ElementRef } from '@angular/core';
 
 export class EventMenuDirective implements OnInit {
 
-  private isOpen = false;
-  private renderer: Renderer2;
-  private elRef: ElementRef;
-  
-  constructor(renderer: Renderer2, elRef: ElementRef) {
-    this.renderer = renderer;
-    this.elRef = elRef;
+  constructor() {
   }
 
-  openMenu(): void {
-    // console.log( 'openMenu' );
-    let elemParent = document.querySelector('.header');
-    let elemChild = document.querySelector('.header__menu');
-    elemChild.classList.add('header__menu--open');
-    elemParent.classList.add('header--open');
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+
+    let headerOpen = document.querySelector('.header--open');
+    let headerMenuOpen = document.querySelector('.header__menu--open');
+
+    if (event.target.innerWidth > 980 && headerOpen !== null && headerMenuOpen !== null) {
+      this.closeMenu(headerOpen, headerMenuOpen);
+    }
   }
 
-  closeMenu(): void {
+  closeMenu(headerOpen: Element, headerMenuOpen: Element): void {
     // console.log( 'closeMenu' );
-    let elemParent = document.querySelector('.header');
-    let elemChild = document.querySelector('.header__menu');
-    elemChild.classList.remove('header__menu--open');
-    elemParent.classList.remove('header--open');
+    headerOpen.classList.remove('header--open');
+    headerMenuOpen.classList.remove('header__menu--open');
+    headerOpen.classList.add('header');
+    headerMenuOpen.classList.add('header__menu');
   }
 
   hasClass(elem: Element, className: string): boolean {
@@ -41,45 +38,6 @@ export class EventMenuDirective implements OnInit {
     return hasClass; 
   }
 
-  ngOnInit(): void {
-
-    console.log( 'isOpen onInit', this.isOpen );
-
-    this.renderer.listen(this.elRef.nativeElement, 'click', () => {
-
-      // https://github.com/angular/angular/issues/19554
-
-      let elemParent: Element = document.querySelector('.header');
-      let elemChild: Element = document.querySelector('.header__menu');
-
-      this.isOpen = elemParent.classList.contains('header--open') && elemChild.classList.contains('header__menu--open');
-
-      console.log( !(this.hasClass(this.elRef.nativeElement, 'header__menu__btn') || this.hasClass(this.elRef.nativeElement, 'header__menu__close')))
-
-      // Manage the --active class
-      if( !(this.hasClass(this.elRef.nativeElement, 'header__menu__btn') || this.hasClass(this.elRef.nativeElement, 'header__menu__close')) ){
-        // Remove --active class of old element
-        let oldActiveElement: Element = document.querySelector('.header__menu__links__item--active');
-        oldActiveElement.classList.remove('header__menu__links__item--active');
-  
-        // Add --active class of current element
-        let currentActiveElementParent = this.renderer.parentNode(this.elRef.nativeElement);
-        this.renderer.addClass(currentActiveElementParent, 'header__menu__links__item--active');
-      }
-
-      // Manage the opening/closing of the menu
-      if(this.isOpen) {
-        this.closeMenu();
-        this.isOpen = false;
-        console.log( 'removeClass' );
-      }else{
-        this.openMenu();
-        this.isOpen = true;
-        console.log( 'addClass' );
-      }
-
-	  });
-
-  }
+  ngOnInit(): void {}
 
 }
