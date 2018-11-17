d559b7feb6a7371f5eb2b31e22b246b16f7624f3
apolaina <polainalice@gmail.com>
2018-11-17 18:00:30 +0100

    [efficient-resume] Add router animations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 const routes: Routes = [
-  { path: '', redirectTo: '/about-me', pathMatch: 'full'},
-  { path: 'about-me', component: AboutMeComponent },
-  { path: 'skills', component: SkillsComponent },
+  { path: '', redirectTo: '/about-me', pathMatch: 'full', data: {animation: 'HomePage'}},
+  { path: 'about-me', component: AboutMeComponent, data: {animation: 'AboutMePage'} },
+  { path: 'skills', component: SkillsComponent, data: {animation: 'SkillsPage'} },
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  query,
+  // ...
+} from '@angular/animations';
+
+import { RouterOutlet } from '@angular/router';
+import { slideInAnimation } from './animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.pug',
-  styleUrls: ['./app.component.styl']
+  styleUrls: ['./app.component.styl'],
+  animations: [
+    slideInAnimation
+    // animation triggers go here
+  ]
 })
 export class AppComponent {
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
+
 }
